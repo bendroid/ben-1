10f934ec6213fd1e4b5824dbd83af13694c463b5
Ben Collins <collins.drums@gmail.com>
2015-07-27 17:57:17 -0400

    updated style added modals

--- js/init.js
+++ js/init.js
@@ -11,6 +11,20 @@
       itemSelector: '.col',
     });
 
+    $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+
+    $('.dropdown-button').dropdown({
+      constrain_width: true, // Does not change width of dropdown to that of the activator
+      hover: false, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true // Displays dropdown below the button
+    }
+  );
+
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
